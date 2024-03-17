0d1b7b8fa197d6663c592dc2bc343ca5766f68e0
Darian Rivera <darianrivera@Darians-MacBook-Pro.local>
2024-03-17 17:41:02 -0400

    Homework 8 Changes

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -20,4 +20,72 @@ class PostController extends Controller
 
     }
 
+    public function savePost()
+    {
+        $id = $_POST['id'] ? $_POST['id'] : false;
+        $username = $_POST['username'] ? $_POST['username'] : false;
+        $content = $_POST['content'] ? $_POST['content'] : false;
+        $error = [];
+
+        if ($id)
+        {
+            if ($id < 0 || $id > 100 || !intval($id))
+            {
+                $errors['ageInvalid'] = 'age is invalid';
+            }
+        }
+        else
+        {
+            $errors['requiredAge'] = 'age is required';
+        }
+
+        if ($username)
+        {
+            $username = htmlspecialchars($username, ENT_QUOTES|ENT_HTML5, 'UTF-8', true);
+
+            if (strlen($username) < 2)
+            {
+                $errors['usernameShort'] = 'username is too short';
+            }
+            else if (strlen($username) > 20)
+            {
+                $errors['usernameLong'] = 'username is too long';
+            }
+        }
+        else
+        {
+            $errors['requiredUsername'] = 'username is required';
+        }
+        
+        if ($content)
+        {
+            $content = htmlspecialchars($content, ENT_QUOTES|ENT_HTML5, 'UTF-8', true);
+
+            if (strlen($content) > 100)
+            {
+                $errors['contentLong'] = 'content is too long';
+            }
+        }
+        else
+        {
+            $errors['contentRequired'] = 'content is required';
+        }
+
+        if (count($errors))
+        {
+            http_response_code(400);
+            echo json_encode($errors);
+            exit();
+        }
+
+        $returnData = [
+            'id' => $id,
+            'username' => $username,
+            'content' => $content,
+        ];
+        echo json_encode($returnData);
+        exit();
+        
+    }
+
 }
--- public/index.php
+++ public/index.php
@@ -11,6 +11,7 @@ use app\controllers\UserController;
 use app\controllers\PostController;
 
 
+$url2 = strtok($_SERVER["REQUEST_URI"], '?');
 $url = $_SERVER["REQUEST_URI"];
 
 //todo add a switch statement router to route based on the url
@@ -18,20 +19,47 @@ $url = $_SERVER["REQUEST_URI"];
 //if it is "/" return the homepage view from the main controller
 //if it is something else return a 404 view from the main controller
 
-$posts = new PostController();
-$main = new MainController();
 
-switch($url){
+if ($url2 === '/posts' && $_SERVER['REQUEST_METHOD'] === 'GET')
+{
+    $posts = new PostController();
+    $posts->index();
+}
+else if ($url2 === '/posts' && $_SERVER['REQUEST_METHOD'] === 'POST')
+{
+    $posts = new PostController();
+    $posts->savePost();
+}
+else if ($url2 === '/' && $_SERVER['REQUEST_METHOD'] === 'GET')
+{
+    $main = new MainController();
+    $main->homepage();
+}
+else
+{
+    $main = new MainController();
+    $main->notFound();
+}
+
+
+
+
+
+/*switch($url){
     case "/posts":
+        $posts = new PostController();
         $posts->index();
         break;
     case "/":
+        $main = new MainController();
         $main->homepage();
         break;
     default:
+        $main = new MainController();
         $main->notFound();
-};
+};*/
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
